ebfb904da7bf61d640164e47a60e16fd06bde857
Eduardo Verde <everde@rollins.edu>
2018-06-19 14:27:49 -0400

    forgot to comment migrate file.

--- PonyORM_example/migrate.py
+++ PonyORM_example/migrate.py
@@ -1,5 +1,11 @@
-# from app import settings
-# from app.models import db
+"""
+Migrate File.
+
+This is simply running the migration function that PonyORM has.
+All we need to do is have the database object and database
+connection paramaters passed.
+
+"""
 from testing_conntection import db_params, db
 
 db.migrate(**db_params)
